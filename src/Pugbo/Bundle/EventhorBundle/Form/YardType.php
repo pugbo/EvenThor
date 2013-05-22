1d536647866cd1a9fdad83a55452b7f2f1cb34c4
Davide Bellettini <davide.bellettini@gmail.com>
2013-05-22 20:16:33 +0200

    Adding Yard form type

---
+++ src/Pugbo/Bundle/EventhorBundle/Form/YardType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pugbo\Bundle\EventhorBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class YardType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('open')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Pugbo\Bundle\EventhorBundle\Entity\Yard'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'pugbo_bundle_eventhorbundle_yardtype';
+    }
+}
