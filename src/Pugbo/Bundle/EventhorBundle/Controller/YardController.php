3d8c8a445635dbb341ea6a521b5e53b38e9eac36
Davide Bellettini <davide.bellettini@gmail.com>
2013-05-22 18:15:58 +0200

    Adding MVC base for Yard->New page

---
+++ src/Pugbo/Bundle/EventhorBundle/Controller/YardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pugbo\Bundle\EventhorBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class YardController
+ * @package Pugbo\Bundle\EventhorBundle\Controller
+ */
+class YardController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function newAction() {return[];}
+}
