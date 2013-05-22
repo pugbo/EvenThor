5023df5787691a3c54275d366d4a895c67f294f7
Davide Bellettini <davide.bellettini@gmail.com>
2013-05-22 20:04:47 +0200

    Yard entity

---
+++ src/Pugbo/Bundle/EventhorBundle/Entity/Yard.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Pugbo\Bundle\EventhorBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Yard
+ *
+ * @ORM\Table(name="yards")
+ * @ORM\Entity(repositoryClass="Pugbo\Bundle\EventhorBundle\Entity\YardRepository")
+ */
+class Yard
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=4000)
+     */
+    private $description;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="open", type="boolean")
+     */
+    private $open;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param  string $title
+     * @return Yard
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param  string $description
+     * @return Yard
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set open
+     *
+     * @param  boolean $open
+     * @return Yard
+     */
+    public function setOpen($open)
+    {
+        $this->open = $open;
+
+        return $this;
+    }
+
+    /**
+     * Get open
+     *
+     * @return boolean
+     */
+    public function getOpen()
+    {
+        return $this->open;
+    }
+}
